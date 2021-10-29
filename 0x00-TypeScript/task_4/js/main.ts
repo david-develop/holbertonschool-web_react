d318cb27b5ae8040d0c0bc5f2ec54c3fa8049563
david-develop <david.peraltar@ab-inbev.com>
2021-10-29 17:13:08 -0500

    4

--- 0x00-TypeScript/task_4/js/main.ts
+++ 0x00-TypeScript/task_4/js/main.ts
@@ -1,7 +1,7 @@
-/// <reference path='./subjects/Subject.ts' />
 /// <reference path='./subjects/Cpp.ts' />
 /// <reference path='./subjects/Java.ts' />
 /// <reference path='./subjects/React.ts' />
+/// <reference path='./subjects/Subject.ts' />
 /// <reference path='./subjects/Teacher.ts' />
 
 export const cpp: Subjects.Cpp = new Subjects.Cpp();
@@ -14,17 +14,17 @@ export const cTeacher: Subjects.Teacher = {
   experienceTeachingC: 10,
 };
 
-console.log("C++");
+console.log('C++');
 cpp.setTeacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-console.log("Java");
+console.log('Java');
 java.setTeacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-console.log("React");
+console.log('React');
 react.setTeacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
