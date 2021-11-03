f06b1d6c6f1aec2f0f925cfa673eef8a17f278a8
david-develop <david.peraltar@ab-inbev.com>
2021-11-02 19:06:00 -0500

    react with webpack and babel

---
+++ 0x02-react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <div className="container">
+          <p>Login to access the full dashboard</p>
+          <label htmlFor="email">Email: </label>
+          <input type="email" id="email" />
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" />
+          <button>OK</button>
+        </div>
+      </div>
+      <footer>
+        <div className="App-footer">
+          <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
