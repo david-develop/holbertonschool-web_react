645fe6e6b39eedbf2d0ceb18400654f530a13b77
david-develop <david.peraltar@ab-inbev.com>
2021-11-02 19:47:12 -0500

    Testing

--- 0x02-react_intro/task_5/dashboard/src/App/App.js
+++ 0x02-react_intro/task_5/dashboard/src/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './holberton_logo.jpg';
+import logo from '../assets/holberton_logo.jpg';
 import './App.css';
-import { getFullYear, getFooterCopy } from './utils';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
