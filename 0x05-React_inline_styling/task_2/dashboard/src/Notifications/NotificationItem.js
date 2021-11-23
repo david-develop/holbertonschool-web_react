84e0f8a7442941e44d0f431d75e78a518e719434
david-develop <david.peraltar@ab-inbev.com>
2021-11-22 22:06:54 -0500

    Modify the NotificationItem.test

---
+++ 0x05-React_inline_styling/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    let element;
+    let typeStyle = css(type === "urgent" ? styles.urgent : styles.default);
+
+    element = <li className={typeStyle} data-priority={type} onClick={() => markAsRead(id)}>{value}</li>
+    if (html) element = <li
+      className={typeStyle}
+      data-priority={type}
+      dangerouslySetInnerHTML={html}
+      onClick={() => markAsRead(id)}
+    ></li>
+
+    return (
+      element
+    );
+  }
+};
+
+NotificationItem.propTypes = {
+  html: propTypes.shape({
+    __html: propTypes.string
+  }),
+  type: propTypes.string.isRequired,
+  value: propTypes.string,
+  markAsRead: propTypes.func,
+  id: propTypes.number,
+}
+
+NotificationItem.defaultProps = {
+  type: 'default'
+}
+
+const styles = StyleSheet.create({
+  default: {
+    color: "blue",
+  },
+
+  urgent: {
+    color: "red",
+  },
+});
+
+export default NotificationItem;
