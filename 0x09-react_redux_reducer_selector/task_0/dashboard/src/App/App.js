a04d2f0c8d82cb409b4a05712fca064c4f59d4ea
david-develop <david.peraltar@ab-inbev.com>
2021-12-14 21:02:07 -0500

    init files

---
+++ 0x09-react_redux_reducer_selector/task_0/dashboard/src/App/App.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+import { StyleSheet, css } from 'aphrodite';
+import { user, logOut } from "./AppContext";
+import AppContext from "./AppContext";
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const listNotificationsInit = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyCombination = this.handleKeyCombination.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+    this.state = {
+      displayDrawer: false,
+      user,
+      logOut: this.logOut,
+      listNotifications: listNotificationsInit,
+    };
+  }
+
+  handleDisplayDrawer() {
+    this.setState({
+      displayDrawer: true
+    });
+  }
+
+  handleHideDrawer() {
+    this.setState({
+      displayDrawer: false
+    });
+  }
+
+  handleKeyCombination(e) {
+    if (e.key === 'h' && e.ctrlKey) {
+      window.alert('Logging you out');
+      this.state.logOut();
+    }
+  }
+
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    this.setState({ user });
+  }
+
+  markNotificationAsRead(id) {
+    this.setState({
+      listNotifications: this.state.listNotifications.filter((notification) => {
+        return notification.id !== id;
+      }),
+    });
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyCombination);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyCombination);
+  }
+
+  render() {
+    const { logOut, displayDrawer, user, listNotifications } = this.state;
+    const isLoggedIn = user.isLoggedIn;
+
+    const value = { user, logOut };
+
+    return (
+      <AppContext.Provider value={value}>
+        <div className={css(styles.general)}>
+          <Notifications
+            listNotifications={listNotifications}
+            displayDrawer={displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+            markNotificationAsRead={this.markNotificationAsRead} />
+          <div className={css(styles.app)}>
+            <Header />
+          </div>
+          <div className={css(styles.body)}>
+            {!isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login logIn={this.logIn} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+            )}
+          </div>
+          <div className={css(styles.bodyFeed)}>
+            <BodySection title="News from the School">
+              <p>Some Random Text</p>
+            </BodySection>
+          </div>
+
+          <div className={css(styles.footer)}>
+            <Footer />
+          </div>
+        </div>
+      </AppContext.Provider>
+    );
+  }
+}
+
+App.defaultProps = {};
+
+App.propTypes = {};
+
+const cssVars = {
+  mainColor: "#e01d3f",
+};
+
+const styles = StyleSheet.create({
+  general: {
+    fontFamily: "Roboto, sans-serif",
+    margin: "0",
+    padding: "0",
+  },
+
+  app: {
+    borderBottom: `3px solid ${cssVars.mainColor}`,
+  },
+
+  body: {
+    display: "flex",
+    justifyContent: "center",
+    paddingLeft: "2rem",
+    paddingRight: "2rem",
+  },
+
+  bodyFeed: {
+    paddingLeft: "2rem",
+    paddingRight: "2rem",
+  },
+
+  footer: {
+    borderTop: `3px solid ${cssVars.mainColor}`,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    position: "fixed",
+    bottom: 0,
+    fontStyle: "italic",
+  },
+});
+
+export default App;
---
+++ 0x09-react_redux_reducer_selector/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import CourseListRow from './CourseListRow';
+import propTypes from 'prop-types';
+import CourseShape from './CourseShape';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const CourseList = (props) => {
+  const listCourses = props.listCourses;
+
+
+  return (
+    <table id="CourseList" className={css(styles.list)}>
+      <thead>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+      </thead>
+      <tbody>
+        {listCourses.length === 0 && (
+          <CourseListRow
+            textFirstCell="No course available yet"
+            isHeader={false}
+          />
+        )}
+
+        {listCourses.map((course) => (
+          <CourseListRow
+            key={course.id}
+            textFirstCell={course.name}
+            textSecondCell={course.credit}
+            isHeader={false}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
+};
+
+CourseList.propTypes = {
+  listCourses: propTypes.arrayOf(CourseShape)
+};
+
+const cssVars = {
+  borderTableColor: "rgb(170, 170, 170);",
+};
+
+const styles = StyleSheet.create({
+  list: {
+    border: `1px solid ${cssVars.borderTableColor}`,
+    borderCollapse: "collapse",
+    width: "95%",
+    margin: "40px auto 0 auto",
+  },
+});
+
+export default CourseList;
